accedf14e96b5492ef447272f93658e4e7204de3
Julio Maldonado <julio.maldonado.sn@gmail.com>
2022-08-27 04:46:47 -0300

    ADD routes controller

--- controllers/index.Controller.js
+++ controllers/index.Controller.js
@@ -4,6 +4,12 @@ const connection = mysql.createConnection({
     user: 'root',
     database: 'municipalidad'
 });
+function respuesta(estado, dato) {
+    return ({
+        status: estado,
+        data: dato
+    })
+};
 
 /*connection.query(
     'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
@@ -15,18 +21,43 @@ const connection = mysql.createConnection({
 module.exports = {
     login: (req, res, next) => {
         connection.query(
-            'SELECT * FROM `usuarios` ',
-            function (err, results, fields) {
-                console.log(results); // results contains rows returned by server
-                console.log(fields); // fields contains extra meta data about results, if available
+            `SELECT * FROM administradores WHERE username=${req.body.username}`,
+            function (err, results) {
+                console.log(results);
+                if (results.length > 0) {                   //results es la tabla con los resultados
+                    
+                    res.json(respuesta(200, results))       //devuelvo results con un estado 200
+                }
+                else {
+                    res.json(respuesta(404, []))            //deberia informar que no se encontro nada
+                }
             }
         );
-        res.send("holi")
     },
     view: (req, res, next) => {
-
+        /*if (admin)                //chequeo de si es admin
+        {*/
+        connection.query(
+            'SELECT * FROM `usuarios` ',
+            function (err, results) {
+                console.log(results);
+                if (results.length > 0) {                   //results es la tabla con los resultados
+                    res.json(respuesta(200, results))       //devuelvo results con un estado 200
+                }
+                else {
+                    res.json(respuesta(404, []))            //deberia informar que no se encontro nada
+                }
+            }
+        );
+        /*}
+        else
+        {
+            res.json(respuesta(200, 'Login necesario'))     //deberia informar que no esta logueado
+        }*/
     },
     register: (req, res, next) => {
-
+        let sql = `INSERT INTO usuarios (DNI, Nombre, Apellido, Telefono, email, direccion) 
+        VALUES (${req.body.dni}, ${req.body.name}, ${req.body.surname}, ${req.body.phone}, ${req.body.mail}, ${req.body.direction})`; //codigo para insertar un nuevo registro SQL
+        connection.query(sql);                              // ejecuto el insert
     },
 }
